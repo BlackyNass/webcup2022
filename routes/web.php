637bb5577686aa158a7c59579eeb6616b6cf3a7d
Mohamed Moussa <moussa.mohamed@cg976.fr>
2022-05-14 15:30:36 +0300

    maj

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::prefix('admin')->name('admin.')->middleware(['auth', 'verified'])->group( function () {
+    
+});
